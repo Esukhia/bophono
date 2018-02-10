f46aaab28427bc4a6ec0c8f34437e1db9b124c1c
Elie Roux <elie.roux@telecom-bretagne.eu>
2018-02-10 18:37:05 +0100

    super basic example

--- tibpho.py
+++ tibpho.py
@@ -1,10 +1,27 @@
 import sdtrie
+import csv
+
+def get_trie_from_file(filename, type="roots"):
+    trie = sdtrie.Trie()
+    with open(filename, newline='') as csvfile:
+        freader = csv.reader(csvfile)
+        for row in freader:
+            trie.add(row[0], row[1])
+    return trie
+
+roots = get_trie_from_file("data/roots.csv")
+ends = get_trie_from_file("data/ends.csv")
+
+def get_phonetics(str):
+    global roots, ends
+    rootinfo = roots.get_longest_match_with_data(str)
+    if not rootinfo:
+        return None
+    enddata = ends.get_data(str[rootinfo['i']:])
+    if not enddata:
+        return None
+    return rootinfo['d']+enddata
 
 if __name__ == '__main__':
     """ Example use """
-    trie = sdtrie.Trie()
-    trie.add("test", "test_data")
-    trie.add("te", "te_data")
-    print(trie.get_longest_match_with_data("test"))
-    print(trie.get_data("test"))
-    print(trie.get_longest_match_with_data("tes"))
+    print(get_phonetics('ཀྱང'))
