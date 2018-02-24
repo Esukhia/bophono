4bcbce66f1586e16c3a00aba32bac01988e7dc46
Elie Roux <elie.roux@telecom-bretagne.eu>
2018-02-24 19:11:27 +0100

    first full IPA results

--- phonengine.py
+++ phonengine.py
@@ -36,12 +36,13 @@ class PhonStateNT:
         'h': 'h', #p. 441
         'm': 'm', #p. 441
         'n': 'n', #p. 442
+        'ny': 'ɲ', #p. 442
         'ng': 'ŋ', #p. 442
         'w': 'w', #p. 443
         'y': 'j' #p. 443
     }
 
-    simpleEndVowMapping = {
+    simpleVowMapping = {
         'ä': 'ɛ', #p. 443
         'ö': 'ø', #p. 444
         'u': 'u', #p. 444
@@ -53,7 +54,7 @@ class PhonStateNT:
         "'": 'ʔ', #p. 435
         ":": 'ː', #p. 435
         'm': 'm', #p. 444
-        'ng': 'ŋ', #p. 442
+        'ng': 'ŋ' #p. 442
     }
 
     def getNextRootCommonPattern(position, tone, lastcondition, phon1, phon2, phon3):
@@ -70,6 +71,8 @@ class PhonStateNT:
             nrc = nrc[1:]
         if nrc in PhonStateNT.simpleRootMapping:
             return PhonStateNT.simpleRootMapping[nrc]
+        if nrc == '':
+            return ''
         if nrc == 'k':
             lastcond = (lastfinal == 'p')
             return PhonStateNT.getNextRootCommonPattern(pos, tone, lastcond, 'k', 'g', 'g̥')
@@ -94,7 +97,7 @@ class PhonStateNT:
         print("unknown root consonant: "+nrc)
         return nrc
 
-    def doCombineCurEnd(self, endofword, rootconsonant='', nextvowel=''):
+    def doCombineCurEnd(self, endofword, nrc='', nextvowel=''): # nrc = next root consonant
         """ combined the self.end into the self.phon """
         if not self.end:
             return
@@ -106,8 +109,81 @@ class PhonStateNT:
         if self.end.endswith('~'):
             self.end = self.end[:-1]
         self.final = PhonStateNT.getFinal(self.end)
+        ## vowels:
+        vowelPhon = ''
+        if self.vowel in PhonStateNT.simpleVowMapping:
+            vowelPhon = PhonStateNT.simpleVowMapping[self.vowel]
+        elif self.vowel == 'a':
+            if self.position == 1 and self.final != 'p':
+                vowelPhon = 'a'
+            else:
+                vowelPhon = 'ə'
+        elif self.vowel == 'e':
+            if self.final != '':
+                vowelPhon = 'ɛ'
+            else:
+                vowelPhon = 'e'
+        elif self.vowel == 'o':
+            if self.final != '':
+                vowelPhon = 'ɔ'
+            else:
+                vowelPhon = 'o'
+        else:
+            print("unknown vowel: "+self.vowel)
+        # add w at beginning of low tone words:
+        if self.position == 1 and self.vowel in "öou" and self.phon == '':
+            self.phon += 'w'
+        self.phon += vowelPhon
+        if self.position == 1:
+            self.phon += self.tone == '+' and '\u02CA' or '\u02CB'
         # TODO: option for r and l, replace : with ː
-        self.phon += "v"
+        finalPhon = ''
+        if self.final in PhonStateNT.simpleFinalMapping:
+            vowelPhon = PhonStateNT.simpleFinalMapping[self.final]
+        elif self.final == 'k':
+            if not endofword: # p. 433
+                if nrc in ['p', 't', 'tr', 'ts', 'c', 's']:
+                    finalPhon = 'k'
+                elif self.vowel in ['i', 'e'] and nrc in ['l', 'sh']:
+                    finalPhon = 'k'
+                elif nrc in ['r', 'l']:
+                    finalPhon = 'g̥'
+                elif self.vowel in ['o', 'u', 'a'] and nrc in ['l', 'sh', 'm', 'ny', 'n', 'ng']:
+                    finalPhon = 'ɣ'
+                elif nrc in ['k', 'ky', 'w', 'y']:
+                    finalPhon = ''
+                elif self.vowel in ['e', 'i'] and nrc in ['m', 'ny', 'n', 'ng']:
+                    finalPhon = 'ŋ'
+                else:
+                    print("unhandled case, this shouldn't happen")
+            else:
+                finalPhon = 'ʔ'
+        elif self.final == 'p':
+            if not endofword:
+                if nrc in ['p', 't', 'tr', 'ts', 'c', 's', 'sh']:
+                    finalPhon = 'p'
+                else:
+                    finalPhon = '' # TODO: or 'b̥'?
+            else:
+                finalPhon = 'ʔ' # TODO: option
+        elif self.final == 'n':
+            if not endofword:
+                if nrc in ['t', 'tr']:
+                    finalPhon = 'n'
+                elif nrc == 'p':
+                    finalPhon = 'm'
+                elif nrc == 'k':
+                    finalPhon = 'ŋ'
+                elif nrc == 'ky':
+                    finalPhon = 'ɲ'
+                else:
+                    finalPhon = 'n' # TODO: or ''?
+            else:
+                finalPhon = '' # TODO: or '~'?
+        else:
+            print("unrecognized final: "+self.final)
+        self.phon += finalPhon
+
 
     def combineWith(self, nextroot, nextend):
         self.position += 1
